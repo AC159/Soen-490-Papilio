789e5bcdbf17335be74cbd0a83a4a1c8e48d9069
Anastassy Cap <anacap100@gmail.com>
2022-09-27 08:27:37 -0400

    #19 users can now login with google

--- app/src/main/java/com/soen490chrysalis/papilio/LoginActivity.kt
+++ app/src/main/java/com/soen490chrysalis/papilio/LoginActivity.kt
@@ -1,12 +1,25 @@
 package com.soen490chrysalis.papilio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.soen490chrysalis.papilio.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityLoginBinding
+
+    private val RC_SIGN_IN = 9001
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +27,77 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        // Initialize Firebase Auth
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.llSignInWithGoogleBtn.setOnClickListener {
+            val intent = googleSignInClient.signInIntent
+            startActivityForResult(intent, RC_SIGN_IN)
+        }
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = firebaseAuth.currentUser
+
+        if ( currentUser != null )
+        {
+            Log.d(Log.DEBUG.toString(), "Current user: ${currentUser.displayName}")
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
+                Log.d(Log.DEBUG.toString(), "firebaseAuthWithGoogle: " + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(Log.DEBUG.toString(), "Google sign in failed", e)
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(Log.DEBUG.toString(), "signInWithCredential:success")
+                    val user = firebaseAuth.currentUser
+                    Log.d(Log.DEBUG.toString(), "Current user: $user")
+
+                    // Go to main page
+                    val homePage = Intent(this, MainActivity::class.java)
+                    startActivity(homePage)
+                    finish()
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(Log.DEBUG.toString(), "signInWithCredential:failure", task.exception)
+                }
+            }
+    }
+
 }
--- app/src/main/java/com/soen490chrysalis/papilio/MainActivity.kt
+++ app/src/main/java/com/soen490chrysalis/papilio/MainActivity.kt
@@ -2,10 +2,30 @@ package com.soen490chrysalis.papilio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.soen490chrysalis.papilio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var firebaseUser : FirebaseUser
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // Initialize Firebase Auth
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        // the user should be already signed in
+        firebaseUser = firebaseAuth.currentUser!!
+
+        binding.tvMessage.text = "Hello ${firebaseUser.displayName}!"
+
     }
 }

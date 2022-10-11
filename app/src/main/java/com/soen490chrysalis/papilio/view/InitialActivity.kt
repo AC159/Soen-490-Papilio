5aaef044c979fb331b9ba124bec7e1b8e12a451e
Anastassy Cap <anacap100@gmail.com>
2022-10-11 14:57:57 -0400

    PAPILIO-19 Renamed initial activity

--- app/src/main/java/com/soen490chrysalis/papilio/view/InitialActivity.kt
+++ app/src/main/java/com/soen490chrysalis/papilio/view/InitialActivity.kt
@@ -3,15 +3,15 @@ package com.soen490chrysalis.papilio.view
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.soen490chrysalis.papilio.databinding.ActivityInitialPageBinding
+import com.soen490chrysalis.papilio.databinding.ActivityInitialBinding
 
-class InitialPage : AppCompatActivity() {
-    private lateinit var binding: ActivityInitialPageBinding
+class InitialActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityInitialBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityInitialPageBinding.inflate(layoutInflater)
+        binding = ActivityInitialBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.loginButton.setOnClickListener {

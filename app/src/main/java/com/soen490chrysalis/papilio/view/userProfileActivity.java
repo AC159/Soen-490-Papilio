b57b87ff357de6e578a32199125296f56838f3cf
BunnyPrince <jananee1999@yahoo.com>
2022-10-15 21:15:42 -0400

    the user profile page created

---
+++ app/src/main/java/com/soen490chrysalis/papilio/view/userProfileActivity.java
@@ -0,0 +1,17 @@
+package com.soen490chrysalis.papilio.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.soen490chrysalis.papilio.R;
+
+public class userProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}

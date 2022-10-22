d551f2bc7a10e7978feb79bcd1e05633d3d4dc3d
Anas Peerzada <apeerzada987@gmail.com>
2022-10-22 01:24:16 -0400

    account menu page (in progress)

--- app/src/main/java/com/soen490chrysalis/papilio/view/InitialActivity.kt
+++ app/src/main/java/com/soen490chrysalis/papilio/view/InitialActivity.kt
@@ -2,6 +2,7 @@ package com.soen490chrysalis.papilio.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.soen490chrysalis.papilio.databinding.ActivityInitialBinding
 
@@ -23,6 +24,8 @@ class InitialActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+
+
 
     }
 

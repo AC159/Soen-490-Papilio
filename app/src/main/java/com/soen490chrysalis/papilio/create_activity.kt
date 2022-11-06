496e6cce9f7979d295f797cee1d0648b53cae1a1
Zain-MKhan <zmkhan025@gmail.com>
2022-11-06 01:01:31 -0500

    PAPILIO-23-create-activity-ui

---
+++ app/src/main/java/com/soen490chrysalis/papilio/create_activity.kt
@@ -0,0 +1,11 @@
+package com.soen490chrysalis.papilio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class create_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_activity)
+    }
+}

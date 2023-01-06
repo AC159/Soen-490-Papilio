f3f7ac5b8e3c00f7157236c9641fa9a408567ea6
Anas Peerzada <apeerzada987@gmail.com>
2023-01-05 22:24:55 -0500

    Added User Profile Phone and Password Editing

--- app/src/main/java/com/soen490chrysalis/papilio/view/UserProfileActivity.kt
+++ app/src/main/java/com/soen490chrysalis/papilio/view/UserProfileActivity.kt
@@ -1,23 +1,29 @@
 package com.soen490chrysalis.papilio.view
 
+import android.app.ActionBar.LayoutParams
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.graphics.Color
 import android.os.Bundle
+import android.text.InputType
 import android.view.MenuItem
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.soen490chrysalis.papilio.*
 import com.soen490chrysalis.papilio.databinding.ActivityUserProfileBinding
 import com.soen490chrysalis.papilio.repository.users.IUserRepository
 import com.soen490chrysalis.papilio.repository.users.UserRepository
 import com.soen490chrysalis.papilio.services.network.UserApi
-import com.soen490chrysalis.papilio.viewModel.LoginViewModel
-import com.soen490chrysalis.papilio.viewModel.LoginViewModelFactory
 import com.soen490chrysalis.papilio.viewModel.UserProfileViewModel
 
 
@@ -26,7 +32,8 @@ class UserProfileActivity : AppCompatActivity()
     private lateinit var binding : ActivityUserProfileBinding
     private lateinit var userProfileViewModel : UserProfileViewModel
     private var isEditing : Boolean = false
-    private var editedFields : MutableMap<String, kotlin.Any> = mutableMapOf<String, kotlin.Any>()
+    private var isGoogleAccount : Boolean = false
+
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -47,6 +54,8 @@ class UserProfileActivity : AppCompatActivity()
         // If the Google account exists (which means the user used Google to log in / sign up
         if(acct != null)
         {
+            isGoogleAccount = true
+
             // Get the photoUrl of the google profile picture of your Google account
             var googleProfilePicture = acct.photoUrl
 
@@ -71,6 +80,10 @@ class UserProfileActivity : AppCompatActivity()
             userProfileViewModel.userObject.observe(this, Observer {
                 binding.userProfileBio.setText(userProfileViewModel.userObject.value?.userObject?.bio)
                 binding.userProfileBioEdit.setText(userProfileViewModel.userObject.value?.userObject?.bio)
+                binding.userProfilePhone.setText("Phone: +"
+                        + userProfileViewModel.userObject.value?.userObject?.countryCode
+                        + " "
+                        + userProfileViewModel.userObject.value?.userObject?.phone)
             })
 
         }
@@ -79,9 +92,8 @@ class UserProfileActivity : AppCompatActivity()
             // Get the firebase user from FireBaseAuth
             val user = FirebaseAuth.getInstance().currentUser
             user?.let{
+
                 // Set the full name, email on the profile from the firebase account, and show the placeholder pfp as the pfp
-
-
 
                 binding.userProfileName.setText(user.displayName)
                 binding.userProfileEmail.setText("Email: " + user.email)
@@ -90,10 +102,21 @@ class UserProfileActivity : AppCompatActivity()
                 userProfileViewModel.userObject.observe(this, Observer {
                     binding.userProfileBio.setText(userProfileViewModel.userObject.value?.userObject?.bio)
                     binding.userProfileBioEdit.setText(userProfileViewModel.userObject.value?.userObject?.bio)
+
+                    var countryCode = userProfileViewModel.userObject.value?.userObject?.countryCode
+                    var phone = userProfileViewModel.userObject.value?.userObject?.phone
+
+                    if(phone != null)
+                    {
+                        binding.userProfileAddPhoneButton.visibility = View.GONE
+                        binding.userProfilePhoneLayout.visibility = View.VISIBLE
+                        binding.userProfilePhone.setText("Phone: +"
+                                + countryCode
+                                + " "
+                                + phone)
+                    }
                 })
-
             }
-
         }
 
         // Create Action Bar var so we can 1) display it with a proper title and 2) put a working back button on it
@@ -106,16 +129,26 @@ class UserProfileActivity : AppCompatActivity()
             actionBar.title = "User Profile"
         }
 
+        // listener for the "Edit Profile" button on the user profile page.
         binding.editProfileButton.setOnClickListener{
-            if(!isEditing)
+            if(!isEditing) // if we are not already in editing mode, change the text of the button and make all editable fields visible and available for editing
             {
                 binding.editProfileButton.text="Save Changes"
                 binding.userProfileBioEdit.visibility = View.VISIBLE
+                binding.userProfilePhoneEditButton.visibility = View.VISIBLE
                 binding.userProfileBio.visibility = View.GONE
+                binding.userProfilePicture.setImageResource(R.drawable.user_pfp_example_editing)
 
-                isEditing = true
+                if(!isGoogleAccount) // only display the "Change Password" button if the current account is a Google account and not just a database/Firebase account.
+                {
+                    binding.userProfilePasswordButton.visibility = View.VISIBLE
+                }
+
+                isEditing = true // now we are in editing mode
             }
-            else {
+            else { // if we are already in editing mode, then save the changes made by the user to their profile, and call the viewmodel function that sends this info through a PUT request
+
+                val error : String? = null
 
                 if (this.currentFocus != null) {
                     val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
@@ -124,21 +157,211 @@ class UserProfileActivity : AppCompatActivity()
                 binding.userProfileBioEdit.visibility = View.GONE
                 binding.userProfileBio.visibility = View.VISIBLE
                 binding.editProfileButton.text="Edit Profile"
+                binding.userProfilePicture.setImageResource(R.drawable.user_pfp_example)
 
-                isEditing = false
+                if(!isGoogleAccount)
+                {
+                    binding.userProfilePasswordButton.visibility = View.GONE
+                }
+
+                isEditing = false // No longer in editing mode because changes were saved
+
+                // The following if-statements are all about adding the edited fields to a map in the view model
 
                 if(binding.userProfileBio.text != binding.userProfileBioEdit.text)
                 {
-                    editedFields.put("bio", binding.userProfileBioEdit.text.toString())
+                    userProfileViewModel.addEditedField("bio", binding.userProfileBioEdit.text.toString())
                 }
 
-                if(!editedFields.isEmpty())
+
+                // If at least one field was edited by the user, then send the PUT request to the backend so that the user's changes can be saved
+                if(!userProfileViewModel.isEditedFieldsEmpty())
                 {
-                    userProfileViewModel.updateUserProfile(editedFields)
-                    editedFields.clear()
+                    userProfileViewModel.updateUserProfile()
                 }
             }
         }
+
+        binding.userProfilePasswordButton.setOnClickListener{
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Change Password")
+
+            val passwordInput1 = EditText(this)
+            passwordInput1.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            passwordInput1.hint = "Enter Current Password"
+
+            val passwordInput2 = EditText(this)
+            passwordInput2.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            passwordInput2.hint = "Enter New Password"
+
+            val passwordInput3 = EditText(this)
+            passwordInput3.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            passwordInput3.hint = "Confirm New Password"
+
+            val messageText = TextView(this)
+            messageText.setTextColor(Color.RED)
+
+            val layout = LinearLayout(this)
+
+            val params = LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+            )
+
+            layout.orientation = LinearLayout.VERTICAL
+            layout.setPadding(50, 75, 50, 75)
+            layout.layoutParams = params
+
+            layout.addView(passwordInput1)
+            layout.addView(passwordInput2)
+            layout.addView(passwordInput3)
+            layout.addView(messageText)
+
+
+            builder.setView(layout)
+
+            builder.setPositiveButton(
+                "Save",
+                DialogInterface.OnClickListener { dialog_ : DialogInterface, _ : Int ->
+
+                })
+
+            builder.setNegativeButton(
+                "Cancel",
+                DialogInterface.OnClickListener { dialog : DialogInterface, _ : Int ->
+                    run {
+                        // When the "Accept button is pressed, simply dismiss the dialog
+                        dialog.dismiss()
+                    }
+                })
+
+            val dialog = builder.create()
+
+            dialog.show()
+
+            dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(View.OnClickListener {
+
+                var closeDialog = false
+
+
+                var validationError : String? = null
+                var confirmationError : String? = null
+
+                validationError = userProfileViewModel.validatePassword(passwordInput2.text.toString())
+                confirmationError = userProfileViewModel.confirmPassword(passwordInput2.text.toString(), passwordInput3.text.toString())
+
+                if(validationError != null)
+                {
+                    messageText.setText(validationError)
+                }
+                else if (confirmationError != null)
+                {
+                    messageText.setText(confirmationError)
+                }
+                else if(passwordInput1.text == null)
+                {
+                    messageText.setText("Please enter current password")
+                }
+                else
+                {
+                    userProfileViewModel.changeUserPassword(passwordInput1.text.toString(), passwordInput3.text.toString())
+                    closeDialog = true
+                }
+
+                if (closeDialog) dialog.dismiss()
+            })
+
+        }
+
+        binding.userProfileAddPhoneButton.setOnClickListener{
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Change Password")
+
+            val countryCodeInput = EditText(this)
+            countryCodeInput.inputType = InputType.TYPE_CLASS_NUMBER
+
+            val phoneInput = EditText(this)
+            phoneInput.inputType = InputType.TYPE_CLASS_PHONE
+
+            val messageText = TextView(this)
+            messageText.setTextColor(Color.RED)
+
+            val layout = LinearLayout(this)
+
+            val params = LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+            )
+
+            layout.orientation = LinearLayout.VERTICAL
+            layout.setPadding(50, 75, 50, 75)
+            layout.layoutParams = params
+
+            layout.addView(countryCodeInput)
+            layout.addView(phoneInput)
+            layout.addView(messageText)
+
+            builder.setView(layout)
+
+            builder.setPositiveButton(
+                "Save",
+                DialogInterface.OnClickListener { dialog_ : DialogInterface, _ : Int ->
+
+                })
+
+            builder.setNegativeButton(
+                "Cancel",
+                DialogInterface.OnClickListener { dialog_ : DialogInterface, _ : Int ->
+                    run {
+                        // When the "Accept button is pressed, simply dismiss the dialog
+                        dialog_.dismiss()
+                    }
+                })
+
+            val dialog = builder.create()
+
+            dialog.show()
+
+            dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(View.OnClickListener {
+
+                var closeDialog = false
+
+                val validationError1 = userProfileViewModel.validateCountryCode(countryCodeInput.text.toString())
+                val validationError2 = userProfileViewModel.validatePhoneNumber(phoneInput.text.toString())
+
+                if(validationError1 != null)
+                {
+                    messageText.setText(validationError1)
+                }
+                else if (validationError2 != null)
+                {
+                    messageText.setText(validationError2)
+                }
+                else
+                {
+                    userProfileViewModel.addEditedField("countryCode", countryCodeInput.text.toString())
+                    userProfileViewModel.addEditedField("phone", phoneInput.text.toString())
+                    closeDialog = true
+                }
+
+                if (closeDialog) dialog.dismiss()
+            })
+
+        }
+
+        userProfileViewModel.passwordChangeResult.observe(this, Observer {
+            if(userProfileViewModel.passwordChangeResult.value != null)
+            {
+                val coordinatorLayout = binding.relativeLayoutUserProfile
+                displaySnackBar(coordinatorLayout, userProfileViewModel.passwordChangeResult.value.toString())
+            }
+        })
+    }
+
+    // Utility function that displays a snackbar in case of errors
+    private fun displaySnackBar(coordinatorLayout : RelativeLayout, errorMessage : String)
+    {
+        Snackbar.make(coordinatorLayout, errorMessage, Snackbar.LENGTH_LONG).show()
     }
 
     // This is the function that's called when the back button on the action bar is pressed
--- app/src/main/java/com/soen490chrysalis/papilio/viewModel/UserProfileViewModel.kt
+++ app/src/main/java/com/soen490chrysalis/papilio/viewModel/UserProfileViewModel.kt
@@ -1,22 +1,20 @@
 package com.soen490chrysalis.papilio.viewModel
 
 import android.util.Log
-import androidx.core.util.PatternsCompat
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.EmailAuthProvider
+import com.google.firebase.auth.FirebaseAuth
 import com.soen490chrysalis.papilio.repository.users.IUserRepository
-import com.soen490chrysalis.papilio.services.network.responses.GetUserByFirebaseIdResponse
-import com.soen490chrysalis.papilio.services.network.responses.UserObject
 import kotlinx.coroutines.launch
 import java.util.regex.Pattern
 
 class UserProfileViewModel(private val userRepository : IUserRepository) : ViewModel()
 {
     var userObject : MutableLiveData<GetUserResponse> = MutableLiveData<GetUserResponse>()
+    var passwordChangeResult : MutableLiveData<String> = MutableLiveData<String>()
+    private var editedFields : MutableMap<String, kotlin.Any> = mutableMapOf<String, kotlin.Any>()
 
     fun getUserByFirebaseId()
     {
@@ -26,16 +24,110 @@ class UserProfileViewModel(private val userRepository : IUserRepository) : ViewM
         }
     }
 
-    fun updateUserProfile(variableMap: Map<String, kotlin.Any>)
+    fun addEditedField(fieldName: String, fieldValue: kotlin.Any)
     {
+        editedFields.put(fieldName, fieldValue);
+    }
+
+    fun isEditedFieldsEmpty() : Boolean
+    {
+        return editedFields.isEmpty()
+    }
+
+    fun updateUserProfile()
+    {
+        if(editedFields.isEmpty()) return
+
         viewModelScope.launch {
-            val userResponse = userRepository.updateUser(variableMap)
+            val userResponse = userRepository.updateUser(editedFields)
 
             val afterUpdateResponse = userRepository.getUserByFirebaseId()
 
             userObject.value = GetUserResponse(true, afterUpdateResponse?.user)
+
+            editedFields.clear()
+        }
+    }
+
+    fun validatePhoneNumber(number : String): String?
+    {
+        if(number.length < 10)
+        {
+            return "Please enter a valid phone number (10 digits)"
         }
 
+        return null
+    }
+
+    fun validateCountryCode(number : String): String?
+    {
+        if(number.length > 5 || number.isEmpty())
+        {
+            return "Please enter a valid country code (without the plus sign)"
+        }
+
+        return null
+    }
+
+    fun validatePassword(password : String) : String?
+    {
+        val passwordREGEX = Pattern.compile(
+            "^" +
+                    "(?=.*[0-9])" +         // at least 1 digit
+                    "(?=.*[a-z])" +         // at least 1 lower case letter
+                    "(?=.*[A-Z])" +         // at least 1 upper case letter
+                    "(?=.*[a-zA-Z])" +      // any letter
+                    "(?=.*[!@#$%^&*()_+])" +// at least 1 special character
+                    "(?=\\S+$)" +           // no white spaces
+                    ".{6,}" +               // at least 6 characters
+                    "$"
+        )
+
+        if (password.length in 6..20 && passwordREGEX.matcher(password).matches())
+        {
+            return null
+        }
+        return "Password must contain at least 1 digit, 1 lowercase character, " +
+                "1 uppercase character, 1 special character, no white spaces & a minimum of 6 characters!"
+    }
+
+    fun confirmPassword(password : String, password2 : String) : String?
+    {
+        if(password == password2)
+            return null
+        else
+            return "Make sure passwords match!"
+    }
+
+    fun changeUserPassword(oldPassword : String, newPassword : String)
+    {
+        viewModelScope.launch {
+
+            val user = FirebaseAuth.getInstance().currentUser
+            val credential = user!!.email?.let { EmailAuthProvider.getCredential(it, oldPassword) }
+
+            if (credential != null) {
+                user!!.reauthenticate(credential)
+                    .addOnCompleteListener { task ->
+                        if(task.isSuccessful) {
+                            user!!.updatePassword(newPassword).addOnCompleteListener { task ->
+                                if(task.isSuccessful) {
+                                    passwordChangeResult.value = "Password changed successfully!"
+                                    Log.e("updatePassword", "success", task.exception)
+                                } else {
+                                    passwordChangeResult.value = "Error! Couldn't change password"
+                                    Log.e("updatePassword", "error", task.exception)
+                                }
+                            }
+                        } else {
+                            passwordChangeResult.value = "Error! Couldn't authenticate user"
+                            Log.e("updatePassword", "error", task.exception)
+                        }
+                    }
+            }
+
+
+        }
 
     }
 }

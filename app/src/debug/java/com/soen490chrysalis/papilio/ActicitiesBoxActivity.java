cedf57d5e1245c577915e33605334c1d47ff3787
BunnyPrince <jananee1999@yahoo.com>
2023-01-17 17:50:02 -0500

    PAPILIO-71 the activity box to be used on the list of all the activity feeds is created
    Contains:
    - title
    - image
    - address
    - start time

---
+++ app/src/debug/java/com/soen490chrysalis/papilio/ActicitiesBoxActivity.java
@@ -0,0 +1,14 @@
+package com.soen490chrysalis.papilio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActicitiesBoxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acticities_box);
+    }
+}

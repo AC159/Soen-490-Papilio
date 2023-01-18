850512f6f9bf6508b4831a6d5d5f907778d71ebd
BunnyPrince <jananee1999@yahoo.com>
2023-01-17 19:33:47 -0500

    PAPILIO-71 missing the files activity_display_acitivity_info.xml and DisplayAcitivityInfoActivity.java that was included in the AndroidManifest.xml

---
+++ app/src/main/java/com/soen490chrysalis/papilio/view/DisplayAcitivityInfoActivity.java
@@ -0,0 +1,16 @@
+package com.soen490chrysalis.papilio.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.soen490chrysalis.papilio.R;
+
+public class DisplayAcitivityInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_acitivity_info);
+    }
+}

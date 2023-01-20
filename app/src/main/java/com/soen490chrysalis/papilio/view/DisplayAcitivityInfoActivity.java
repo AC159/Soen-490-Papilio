849e6bf8bc54ba6946372de8f8073742d796a603
BunnyPrince <jananee1999@yahoo.com>
2023-01-19 20:26:37 -0500

    PAPILIO-71 Some information is added to the activity info page - Image - Title - Description - two payment info

--- app/src/main/java/com/soen490chrysalis/papilio/view/DisplayAcitivityInfoActivity.java
+++ app/src/main/java/com/soen490chrysalis/papilio/view/DisplayAcitivityInfoActivity.java
@@ -3,6 +3,7 @@ package com.soen490chrysalis.papilio.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.soen490chrysalis.papilio.R;
 
@@ -12,5 +13,23 @@ public class DisplayAcitivityInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_acitivity_info);
+
+
+        var actionBar = getSupportActionBar();
+
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle("activity Info");
+        }
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == android.R.id.home){
+            this.finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }

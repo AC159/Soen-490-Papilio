c992f66699ad47b24d501a67be1e2287a536c227
Anas Peerzada <apeerzada987@gmail.com>
2023-03-13 21:40:05 -0400

    Addressed Changes as requested in #37
    Changes bug fixes to the favoriting User Flow

--- app/src/main/java/com/soen490chrysalis/papilio/viewModel/ActivityInfoViewModel.kt
+++ app/src/main/java/com/soen490chrysalis/papilio/viewModel/ActivityInfoViewModel.kt
@@ -5,6 +5,9 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.soen490chrysalis.papilio.repository.users.IUserRepository
+import com.soen490chrysalis.papilio.services.network.responses.CheckFavoriteResponse
+import com.soen490chrysalis.papilio.services.network.responses.FavoriteActivitiesResponse
+import com.soen490chrysalis.papilio.services.network.responses.FavoriteResponse
 import kotlinx.coroutines.launch
 
 data class APIResponse(var isSuccess : Boolean, var errorMessage : String)
@@ -20,6 +23,12 @@ class ActivityInfoViewModel(private val userRepository : IUserRepository) : View
     var checkActivityMemberResponse = MutableLiveData<CheckActivityMember>()
     var jonActivityResponse = MutableLiveData<APIResponse>()
     var leaveActivityResponse = MutableLiveData<APIResponse>()
+    var activitiesResponse: MutableLiveData<FavoriteActivitiesResponse> =
+        MutableLiveData<FavoriteActivitiesResponse>()
+    var checkActivityFavoritedResponse: MutableLiveData<CheckFavoriteResponse> =
+        MutableLiveData<CheckFavoriteResponse>()
+    var activityFavoritedResponse: MutableLiveData<FavoriteResponse> =
+        MutableLiveData<FavoriteResponse>()
 
     // Function that fetches information about whether or not a user has joined an activity
     fun checkActivityMember(activity_id : String)
@@ -47,6 +56,54 @@ class ActivityInfoViewModel(private val userRepository : IUserRepository) : View
             val result = userRepository.removeUserFromActivity(activity_id)
             Log.d(logTag, "response from leaveActivity(): $result")
             leaveActivityResponse.value = APIResponse(result.first, result.second)
+        }
+    }
+
+    fun checkActivityFavorited(activityId: Number) {
+        viewModelScope.launch {
+            try {
+                val getActivityResponse = userRepository.isActivityFavorited(activityId.toString())
+                checkActivityFavoritedResponse.value = CheckFavoriteResponse(
+                    getActivityResponse.body()!!.isActivityFound
+                )
+                Log.d("checkActivityFavorited", activitiesResponse.value.toString())
+            } catch (e: Exception) {
+                Log.d(logTag, "userRepository.checkActivityFavorited - exception:\n $e")
+            }
+        }
+    }
+
+    fun addFavoriteActivity(activityId: Number) {
+        viewModelScope.launch {
+            try {
+                val getActivityResponse = userRepository.addFavoriteActivity(activityId)
+
+                Log.d("addFavoriteActivity", getActivityResponse.message())
+
+                activityFavoritedResponse.value = FavoriteResponse(
+                    getActivityResponse.body()?.success,
+                    getActivityResponse.body()?.update
+                )
+            } catch (e: Exception) {
+                Log.d(logTag, "userRepository.addFavoriteActivity - exception:\n $e")
+            }
+        }
+    }
+
+    fun removeFavoriteActivity(activityId: Number) {
+        viewModelScope.launch {
+            try {
+                val getActivityResponse = userRepository.removeFavoriteActivity(activityId)
+
+                Log.d("removeFavoriteActivity", getActivityResponse.message())
+
+                activityFavoritedResponse.value = FavoriteResponse(
+                    getActivityResponse.body()?.success,
+                    getActivityResponse.body()?.update
+                )
+            } catch (e: Exception) {
+                Log.d(logTag, "userRepository.removeFavoriteActivity - exception:\n $e")
+            }
         }
     }
 }
--- app/src/main/java/com/soen490chrysalis/papilio/viewModel/factories/FavoriteActivitiesViewModelFactory.kt
+++ app/src/main/java/com/soen490chrysalis/papilio/viewModel/factories/FavoriteActivitiesViewModelFactory.kt
@@ -6,7 +6,6 @@ import com.google.firebase.auth.FirebaseAuth
 import com.soen490chrysalis.papilio.repository.users.IUserRepository
 import com.soen490chrysalis.papilio.repository.users.UserRepository
 import com.soen490chrysalis.papilio.services.network.UserApi
-import com.soen490chrysalis.papilio.viewModel.DisplayActivityViewModel
 import com.soen490chrysalis.papilio.viewModel.FavoriteActivitiesViewModel
 
 class FavoriteActivitiesViewModelFactory : ViewModelProvider.NewInstanceFactory()
